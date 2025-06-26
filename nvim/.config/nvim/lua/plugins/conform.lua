92ad8b082caddbd5b2d8ad6d5c1ae2b815ac12d3
Ricardo Ferreira <ricardoferreira@outlook.com>
2025-06-26 19:54:54 +0100

    Fix Stylua indentation with Conform.nvim

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,17 +1,32 @@
 return {
-	"stevearc/conform.nvim",
-	opts = {
-		formatters_by_ft = {
-			lua = { "stylua" },
-			-- You can customize some of the format options for the filetype (:help conform.format)
-			terraform = { "terraform", lsp_format = "fallback" },
-			-- Conform will run the first available formatter
-			javascript = { "prettierd", "prettier", stop_after_first = true },
-		},
-		-- format_on_save = {
-		-- 	-- These options will be passed to conform.format()
-		-- 	timeout_ms = 500,
-		-- 	lsp_format = "fallback",
-		-- },
-	},
+    "stevearc/conform.nvim",
+    opts = {
+        formatters_by_ft = {
+            lua = {
+                "stylua",
+            },
+            -- You can customize some of the format options for the filetype (:help conform.format)
+            terraform = { lsp_format = "fallback" },
+            -- Conform will run the first available formatter
+            javascript = { "prettierd", "prettier", stop_after_first = true },
+        },
+        -- Set this to change the default values when calling conform.format()
+        -- This will also affect the default values for format_on_save/format_after_save
+        default_format_opts = {
+            lsp_format = "fallback",
+        },
+        -- format_on_save = {
+        -- -- These options will be passed to conform.format()
+        -- timeout_ms = 500,
+        -- lsp_format = "fallback",
+        -- },
+        formatters = {
+            stylua = {
+                prepend_args = {
+                    "--indent-type",
+                    "Spaces",
+                },
+            },
+        },
+    },
 }
