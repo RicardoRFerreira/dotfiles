748ce112cdaf4bbf18c4e7ef98f37e2fb35a0c7e
Ricardo Ferreira <ricardoferreira@outlook.com>
2026-05-19 22:57:12 +0100

    Use Lua for Hyprland

---
+++ hypr/.config/hypr/hyprland/autostart.lua
@@ -0,0 +1,9 @@
+-- See https://wiki.hypr.land/Configuring/Basics/Autostart/
+
+-- Autostart necessary processes (like notifications daemons, status bars, etc.)
+-- Or execute your favorite apps at launch like this:
+--
+hl.on("hyprland.start", function ()
+  hl.exec_cmd("hyprpanel & hypridle")
+  -- hl.exec_cmd("kmonad ~/config.kbd")
+end)
