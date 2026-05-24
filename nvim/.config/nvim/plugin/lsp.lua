e273ec79bedd3e9894b01dcfc22add1f397ce0dd
Ricardo Ferreira <ricardoferreira@outlook.com>
2026-05-24 23:17:31 +0100

    fix: Address deprecated rust-analyzer setting

--- nvim/.config/nvim/plugin/lsp.lua
+++ nvim/.config/nvim/plugin/lsp.lua
@@ -163,7 +163,8 @@ local servers = {
   rust_analyzer = {
     settings = {
       ['rust-analyzer'] = {
-        checkOnSave = {
+        checkOnSave = true,
+        check = {
           command = 'clippy',
         },
         cargo = {
